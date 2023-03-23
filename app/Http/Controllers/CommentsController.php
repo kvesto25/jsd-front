b26098ee3eb10482372e5fb56055121619fa91e3
DESKTOP-F1GSKER\kvest <kvesto25@gmail.com>
2023-03-23 10:20:06 +0200

    First commit

---
+++ app/Http/Controllers/CommentsController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CommentsController extends Controller
+{
+    public function index(){
+        $str = "Taras Lavrin";
+        var_dump($str);
+    }
+}
---
+++ database/migrations/2023_03_15_230736_create_comments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_client')->default(0);
+            $table->string('stars')->nullable();
+            $table->string('comment_text')->nullable();
+            $table->integer('id_freelancer')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
